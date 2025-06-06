5e2932748ff407a4a50504abc1a84c187e5dfcda
Chobola <chobola@in.tum.de>
2025-06-06 21:07:41 +0200

    new lucyd gradio logic

--- lucyd3d/lucyd3d_gradio.py
+++ lucyd3d/lucyd3d_gradio.py
@@ -1,12 +1,28 @@
 import torch
 from .model import LUCYD
+import os 
+import numpy as np
 
-def lucyd3d_run(noisy_img):
+def input_image_logic(img):
+    img = img / np.max(img)
+    input_image = torch.from_numpy(img).unsqueeze(0).unsqueeze(0).float().cuda()
+    return input_image
+
+
+def output_image_logic(img):
+    restored_img = img.squeeze(0).squeeze(0).detach().cpu().numpy()
+    return restored_img
+
+
+def lucyd3d_run(noisy_img, weights):
+
+    WEIGHT = os.path.join(os.path.dirname(__file__), weights)
+
     model = LUCYD().cuda()
-    model.load_state_dict(torch.load('./lucyd3d.pth'))
+    model.load_state_dict(torch.load(WEIGHT))
     model.eval()
 
     with torch.no_grad():
         out = model(noisy_img)
 
-    return out
+    return output_image_logic(out)
